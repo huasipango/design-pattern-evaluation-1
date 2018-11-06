a55792a3c15011d16b7f164093c8d3e022e81579
Anthony <huasipango@gmail.com>
2018-11-06 09:52:29 -0500

    creado esqueleto base del proyecto directorios /styles /js e index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Design Pattern Project</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<script type="text/javascript" src="js/main.js"></script>
+</head>
+<body>
+
+</body>
+</html>
